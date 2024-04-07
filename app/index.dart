1fa7b878c086a90c80504994c814d3edcc899ca8
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-04-07 09:38:57 +0530

    Add files via upload

---
+++ app/index.dart
@@ -0,0 +1,19 @@
+// Export pages
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/pages/login_page/login_page_widget.dart' show LoginPageWidget;
+export '/pages/grievances/grievances_widget.dart' show GrievancesWidget;
+export '/pages/raise_grievance/raise_grievance_widget.dart'
+    show RaiseGrievanceWidget;
+export '/pages/sign_in_up_govt/sign_in_up_govt_widget.dart'
+    show SignInUpGovtWidget;
+export '/pages/citizen_profile_create_edit/citizen_profile_create_edit_widget.dart'
+    show CitizenProfileCreateEditWidget;
+export '/pages/govt_profile_create_edit/govt_profile_create_edit_widget.dart'
+    show GovtProfileCreateEditWidget;
+export '/pages/profile/profile_widget.dart' show ProfileWidget;
+export '/pages/sign_in_up_citizen/sign_in_up_citizen_widget.dart'
+    show SignInUpCitizenWidget;
+export '/pages/dashboard_govt/dashboard_govt_widget.dart'
+    show DashboardGovtWidget;
+export '/pages/open_grievance/open_grievance_widget.dart'
+    show OpenGrievanceWidget;
